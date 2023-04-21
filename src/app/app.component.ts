cd00a96b3e765aa6a2343c3ed7b84d448940491d
alexveretenkin <oleksii.veretionkin@inveritasoft.com>
2023-04-21 23:06:49 +0300

    added ui lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NzButtonModule],
   selector: 'pokemon-cards-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
